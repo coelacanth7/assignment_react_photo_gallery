2bc54800ec4f85b7fd8940c5326983501478f3e4
Coelacanth7 <galford151@gmail.com>
2017-12-21 01:25:19 -0700

    built json obj into html

--- src/components/PhotoBody.js
+++ src/components/PhotoBody.js
@@ -2,8 +2,19 @@ import React from "react";
 import PictureCard from "./PictureCard";
 
 const PhotoBody = ({ photos }) => {
-	const pics = photos.map((pic, ind) => {
-		return <PictureCard pic={pic} key={ind} />;
+	const pics = photos.map((pic, index) => {
+		const picObj = {};
+		picObj.caption = pic.caption ? pic.caption.text : "";
+		picObj.comments = pic.comments.data.length;
+		picObj.img = pic.images.standard_resolution.url;
+		picObj.link = pic.link;
+		picObj.likes = pic.likes.count;
+		picObj.tags = pic.tags.join(", ");
+		picObj.username = pic.user.username;
+		picObj.filter = pic.filter;
+		picObj.date = new Date(pic.created_time * 1000).toDateString();
+
+		return <PictureCard picObj={picObj} pic={pic} key={index} />;
 	});
 
 	return <div className="row">{pics}</div>;
--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -1,9 +1,36 @@
 import React from "react";
 
-const PictureCard = ({ photos }) => {
+const PictureCard = ({ picObj }) => {
+	const {
+		comments,
+		img,
+		link,
+		likes,
+		tags,
+		username,
+		filter,
+		date,
+		caption
+	} = picObj;
+
 	return (
 		<div className="col-sm-4">
-			<pre>{photos}</pre>
+			<div className="card">
+				<a href={link}>
+					<img className="card-img-top" src={img} alt="Card image cap" />
+				</a>
+				<div className="card-body">
+					<h4 className="card-title">
+						<a href={`https://www.instagram.com/${{ username }}`}>{username}</a>
+					</h4>
+					<p className="card-text">{caption}</p>
+					<p>{tags}</p>
+					<p>{filter}</p>
+					<p>{date}</p>
+					<p>{likes} likes</p>
+					<p>{comments} comments</p>
+				</div>
+			</div>
 		</div>
 	);
 };
